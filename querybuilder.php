9525f8851fc6f8b36bd52be87fad8ab5248fbc5c
EvanDonovan <evan@evandonovan.org>
2011-06-02 12:27:32 -0400

    Provided example query.

--- querybuilder.php
+++ querybuilder.php
@@ -11,17 +11,24 @@
 <h1>All For Good Query Builder</h1>
 <p>This page uses the All For Good query parameters as documented on <a href="http://www.allforgood.org/api.html">their API page</a>.</p>
 <h2>How to Use the Query (q) Parameter</h2>
-   <ul>
-        <li>When creating a query, list search terms separated by spaces, in the form <code>q=term1 term2 term3</code>. (As with all of the query parameter values, the spaces must be URL encoded.) The service returns all entries that match all of the search terms (like using <code>AND</code> between terms). Like Google's web search, a service searches on complete words (and related words with the same stem), not substrings.</li>
-			<li>Boolean operations may also be used, such as AND and OR.</li>
-			<li>You may use parentheses to group terms, such as (hunger OR food)</li>
+  <ul>
+      <li>When creating a query, list search terms separated by spaces, in the form <code>q=term1 term2 term3</code>. (As with all of the query parameter values, the spaces must be URL encoded.) The service returns all entries that match all of the search terms (like using <code>AND</code> between terms). Like Google's web search, a service searches on complete words (and related words with the same stem), not substrings.</li>
+      <li>Boolean operations may also be used, such as AND and OR.</li>
+      <li>You may use parentheses to group terms, such as (hunger OR food)</li>
       <li>To search for an exact phrase, enclose the phrase in quotation marks: <code>q="exact&nbsp;phrase".</code></li>
       <li>To exclude entries that match a given term, use the form <code>q=-term</code>.</li>
       <li>The search is case-insensitive.</li>
       <li>Example: to search for all entries that contain the exact phrase "Elizabeth Bennet" and the word "Darcy" but don't contain the word "Austen", use the following query: <code>?q="Elizabeth Bennet" Darcy -Austen</code></li>
-			<li>You can filter on particular fields by specifying the field name. For example, <code>-detailUrl: christianvolunteering</code> excludes results where the provider's source URL contains "christianvolunteering".</li>
-    </ul>
+      <li>You can filter on particular fields by specifying the field name. For example, <code>-detailUrl: christianvolunteering</code> excludes results where the provider's source URL contains "christianvolunteering".</li>
+  </ul>
 <p><small>Source: <a href="http://code.google.com/apis/gdata/docs/2.0/reference.html#Queries">Google Data Protocol Reference</a></small>, with additions based on example provided by Dan Stryker of AllForGood</p>
+<h2>Example Query</h2>
+<ul>
+  <li><strong>q:</strong> -detailurl:volunteermatch AND -detailurl:christianvolunteering AND -detailurl:churchvolunteering AND (christian OR jesus OR catholic OR ministry) </li>
+  <li><strong>num:</strong> 500</li>
+  <li><strong>vol_dist:</strong> 500</li>
+  <li><strong>vol_loc:</strong> Boston,MA</li>
+</ul>
 <div id="all-for-good-query-form">
 <form method="post" action="dynamic.php">
 <div class="form-element"><label for="q">Query (q):</label> <input type="text" size="60" name="q" id="q" /></div>
